2192e5fa3197cc36ff266f3fdde5261d5c4ac1f2
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-02 20:48:26 +0800

    made responsive for mobile

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,80 +1,76 @@
 import React from "react";
 import { AiOutlineTwitter } from "react-icons/ai";
 import { BiLogoFacebook } from "react-icons/bi";
-import img from "../assets/logo.jpg";
 import { Link } from "react-router-dom";
+import WelcomePage from "../components/auth/WelcomePage";
 
 const SignIn = () => {
   return (
-    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
-      <div>
-        <h1 className="text-center font-bold text-2xl">Welcome</h1>
-        <img
-          className="w-[200px] md:w-[350px] hidden md:block"
-          src={img}
-          alt="logo"
-        />
-      </div>
-      <div className="md:w-1/3 max-w-sm">
-        <div className="text-center md:text-left">
-          <label className="mr-1">Sign in with</label>
-          <button
-            type="button"
-            className="mx-1 h-9 w-9  rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-[0_4px_9px_-4px_#3b71ca]">
-            <BiLogoFacebook
-              size={20}
-              className="flex justify-center items-center w-full"
-            />
-          </button>
-          <button
-            type="button"
-            className="inlne-block mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca]">
-            <AiOutlineTwitter
-              size={20}
-              className="flex justify-center items-center w-full"
-            />
-          </button>
-        </div>
-        <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-          <p className="mx-4 mb-0 text-center font-semibold text-slate-500">
-            Or
-          </p>
-        </div>
-        <input
-          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-          type="password"
-          placeholder="Password"
-        />
-        <div className="mt-4 flex justify-between font-semibold text-sm">
-          <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
-            <input className="mr-1" type="checkbox" />
-            <span>Remember Me</span>
-          </label>
-          <a
-            className="text-blue-600 hover:text-blue-700 hover:underline hover:underline-offset-4"
-            href="#">
-            Forgot Password?
-          </a>
-        </div>
-        <div className="text-center md:text-left">
-          <button
-            className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
-            type="submit">
-            Sign in
-          </button>
-        </div>
-        <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
-          Don&apos;t have an account?{" "}
-          <Link
-            className="text-red-600 hover:underline hover:underline-offset-4"
-            to="/signup">
-            Register
-          </Link>
+    <section className="h-screen text-[#EFECEC] bg-[#39ad91] flex flex-col md:flex-row">
+      <WelcomePage />
+      <div className="w-full md:w-[50%] px-5 md:px-0 mt-16 md:mt-0 md:h-screen md:bg-[#39ad91] flex md:justify-center md:items-center">
+        <div className="px-10 pb-5 pt-5 bg-white bg-opacity-20 backdrop-blur-lg rounded-md shadow-2xl">
+          <h1 className="text-3xl font-semibold pb-5">Sign In</h1>
+          <input
+            className="text-sm w-full px-4 py-2 bg-transparent outline-none border-b-2 border-[#efecec] placeholder-[#efecec]"
+            type="text"
+            placeholder="Email Address"
+          />
+          <input
+            className="text-sm w-full px-4 py-2 bg-transparent outline-none border-b-2 border-[#efecec] placeholder-[#efecec] mt-4"
+            type="password"
+            placeholder="Password"
+          />
+          <div className="mt-4 flex justify-between font-semibold text-sm">
+            <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
+              <input className="mr-1" type="checkbox" />
+              <span>Remember Me</span>
+            </label>
+            <a
+              className="text-blue-600 hover:text-blue-700 hover:underline hover:underline-offset-4"
+              href="#">
+              Forgot Password?
+            </a>
+          </div>
+          <div className="text-center md:text-left">
+            <button
+              className="mt-4 bg-[#39ad91] shadow-2xl hover:bg-[#58ffd8] px-4 py-2 text-[#efecec] uppercase rounded text-xs tracking-wider"
+              type="submit">
+              Sign in
+            </button>
+          </div>
+          <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
+            Don&apos;t have an account?
+            <Link
+              className="text-red-600 hover:underline hover:underline-offset-4"
+              to="/signup">
+              Register
+            </Link>
+          </div>
+          <div className="text-center md:text-left">
+            <div className="my-2 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+              <p className="mx-4 mb-0 text-center font-semibold text-slate-500">
+                Or
+              </p>
+            </div>
+            <label className="mr-1">Sign in with</label>
+            <button
+              type="button"
+              className="mx-1 h-9 w-9  rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-[0_4px_9px_-4px_#3b71ca]">
+              <BiLogoFacebook
+                size={20}
+                className="flex justify-center items-center w-full"
+              />
+            </button>
+            <button
+              type="button"
+              className="inlne-block mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca]">
+              <AiOutlineTwitter
+                size={20}
+                className="flex justify-center items-center w-full"
+              />
+            </button>
+          </div>
         </div>
       </div>
     </section>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,83 +1,79 @@
 import React from "react";
 import { AiOutlineTwitter } from "react-icons/ai";
 import { BiLogoFacebook } from "react-icons/bi";
-import img from "../assets/logo.jpg";
+import WelcomePage from "../components/auth/WelcomePage";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
   return (
-    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
-      <div>
-        <h1 className="text-center font-bold text-2xl">Welcome</h1>
-        <img
-          className="w-[200px] md:w-[350px] hidden md:block"
-          src={img}
-          alt="logo"
-        />
-      </div>
-      <div className="md:w-1/3 max-w-sm">
-        <div className="text-center md:text-left">
-          <label className="mr-1">Continue with</label>
-          <button
-            type="button"
-            className="mx-1 h-9 w-9  rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-[0_4px_9px_-4px_#3b71ca]">
-            <BiLogoFacebook
-              size={20}
-              className="flex justify-center items-center w-full"
+    <section className="h-screen text-[#EFECEC] bg-[#39ad91] flex flex-col md:flex-row md:justify-center md:items-center">
+      <WelcomePage />
+      <div className="w-full md:w-[50%] px-5 md:px-0 mt-16 md:mt-0  md:h-screen md:bg-[#39ad91] flex md:justify-center md:items-center">
+        <div className="px-10 pb-5 pt-5 bg-[#efecec] bg-opacity-20 backdrop-blur-lg rounded-md shadow-2xl">
+          <h1 className="text-3xl font-semibold pb-5">Sign Up</h1>
+          <div className="flex justify-between gap-2">
+            <input
+              className="flex-1 text-sm w-full px-4 py-2 bg-transparent outline-none border-b-2 border-[#efecec] placeholder-[#efecec]"
+              type="text"
+              placeholder="First Name"
             />
-          </button>
-          <button
-            type="button"
-            className="inlne-block mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca]">
-            <AiOutlineTwitter
-              size={20}
-              className="flex justify-center items-center w-full"
+            <input
+              className="flex-1 text-sm w-full px-4 py-2 bg-transparent outline-none border-b-2 border-[#efecec] placeholder-[#efecec]"
+              type="text"
+              placeholder="Last Name"
             />
-          </button>
-        </div>
-        <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-          <p className="mx-4 mb-0 text-center font-semibold text-slate-500">
-            Or
-          </p>
-        </div>
-        <div className="flex justify-between">
+          </div>
+
           <input
-            className="flex-1 text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
+            className="text-sm w-full px-4 py-2 bg-transparent outline-none border-b-2 border-[#efecec] placeholder-[#efecec]  mt-4"
             type="text"
-            placeholder="First Name"
+            placeholder="Email Address"
           />
           <input
-            className="flex-1 text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
-            type="text"
-            placeholder="Last Name"
+            className="text-sm w-full px-4 py-2 bg-transparent outline-none border-b-2 border-[#efecec] placeholder-[#efecec] mt-4"
+            type="password"
+            placeholder="Password"
           />
-        </div>
 
-        <input
-          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded  mt-4"
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-          type="password"
-          placeholder="Password"
-        />
-
-        <div className="text-center md:text-left">
-          <button
-            className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
-            type="submit">
-            Sign up
-          </button>
-        </div>
-        <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
-          Already have an account?{" "}
-          <Link
-            className="text-red-600 hover:underline hover:underline-offset-4"
-            to="/">
-            Sign in
-          </Link>
+          <div className="text-center md:text-left">
+            <button
+              className="mt-4 bg-[#39ad91] shadow-2xl hover:bg-[#58ffd8] px-4 py-2 text-[#efecec] uppercase rounded text-xs tracking-wider"
+              type="submit">
+              Sign up
+            </button>
+          </div>
+          <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
+            Already have an account?{" "}
+            <Link
+              className="text-red-600 hover:underline hover:underline-offset-4"
+              to="/">
+              Sign in
+            </Link>
+          </div>
+          <div className="text-center md:text-left">
+            <div className="my-2 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+              <p className="mx-4 mb-0 text-center font-semibold text-slate-500">
+                Or
+              </p>
+            </div>
+            <label className="mr-1">Continue with</label>
+            <button
+              type="button"
+              className="mx-1 h-9 w-9  rounded-full bg-blue-600 hover:bg-blue-700 text-[#efecec] shadow-[0_4px_9px_-4px_#3b71ca]">
+              <BiLogoFacebook
+                size={20}
+                className="flex justify-center items-center w-full"
+              />
+            </button>
+            <button
+              type="button"
+              className="inlne-block mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 uppercase leading-normal text-[#efecec] shadow-[0_4px_9px_-4px_#3b71ca]">
+              <AiOutlineTwitter
+                size={20}
+                className="flex justify-center items-center w-full"
+              />
+            </button>
+          </div>
         </div>
       </div>
     </section>
