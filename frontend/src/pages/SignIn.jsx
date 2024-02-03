f0abda19c82f5c8f2f7d00d7d01149296a4f3641
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-03 21:11:13 +0800

    made responsive

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -40,7 +40,7 @@ const SignIn = () => {
             </button>
           </div>
           <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
-            Don&apos;t have an account?
+            Don&apos;t have an account?{" "}
             <Link
               className="text-red-600 hover:underline hover:underline-offset-4"
               to="/signup">
