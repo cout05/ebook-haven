b3d2fc29c92b530676c51bb16b4af4813886011b
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-03 21:09:21 +0800

    updated

--- frontend/src/components/auth/WelcomePage.jsx
+++ frontend/src/components/auth/WelcomePage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import img from "./logo.jpg";
+import img from "../img/logo.jpg";
 
 const WelcomePage = () => {
   return (
-    <div className="md:h-screen flex justify-between items-center md:items-baseline md:flex-col w-screen md:w-[50%] px-4 py-2 md:p-5">
+    <div className="md:h-screen flex gap-4 justify-between md:justify-start items-center md:items-baseline md:flex-col w-screen md:w-[50%] px-4 py-2">
       <h2 className="text-1xl font-semibold">Ebook Haven</h2>
-      <h1 className="text-2xl md:text-4xl hidden md:block px-10 py-5 font-semibold">
+      <h1 className="text-2xl md:text-4xl mt-16 hidden md:block px-10 font-semibold">
         Discover a World of Stories at Your Fingertips
       </h1>
       <img
-        className="w-[50px] md:w-[300px] md:m-auto rounded-lg mt-3"
+        className="w-[50px] shadow-2xl md:w-[300px] md:mx-auto rounded-lg"
         src={img}
         alt="logo"
       />
