395f7d6892cf991e057d2887313c7479c16ab74d
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-11 20:31:26 +0800

    added profile section

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import { MdOutlineAddBox } from "react-icons/md";
 import BooksCard from "../components/dashboard/BooksCard";
 import BooksTable from "../components/dashboard/BooksTable";
+import { IoChevronBack } from "react-icons/io5";
+import img from "../components/img/logo.jpg";
 
 const Dashboard = () => {
   const [books, setBooks] = useState([]);
@@ -26,31 +28,47 @@ const Dashboard = () => {
   }, []);
   return (
     <div>
-      <div className="flex pt-28 justify-center items-center gap-x-4">
+      <Link to="/" className="p-4">
+        <IoChevronBack className="text-4xl cursor-pointer" />
+      </Link>
+      <div className="flex px-2 gap-4 border-b-2 border-black">
         <button
-          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+          className="text-2xl border-b-4 border-blue-950"
           onClick={() => setShowType("table")}>
           Table
         </button>
-        <button
-          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
-          onClick={() => setShowType("card")}>
+        <button className="text-2xl" onClick={() => setShowType("card")}>
           Card
         </button>
       </div>
-      <div className="flex justify-between items-center">
-        <h1 className="text-3xl my-8">Books List</h1>
-        <Link to="/books/create">
-          <MdOutlineAddBox className="text-sky-800 text-4xl" />
-        </Link>
+
+      <div className="flex justify-between gap-4">
+        <div className="p-10">
+          <div>
+            <img
+              className="w-[100px] h-[100px] rounded-full"
+              src={img}
+              alt="logo"
+            />
+          </div>
+          <h1 className="text-2xl"> Welcome</h1>
+        </div>
+        <div className="border w-full">
+          <div className="flex justify-between items-center px-4">
+            <h1 className="text-3xl my-8">Your books</h1>
+            <Link to="/books/create">
+              <MdOutlineAddBox className="text-sky-800 text-4xl" />
+            </Link>
+          </div>
+          {loading ? (
+            <Spinner />
+          ) : showType === "table" ? (
+            <BooksTable books={books} />
+          ) : (
+            <BooksCard books={books} />
+          )}
+        </div>
       </div>
-      {loading ? (
-        <Spinner />
-      ) : showType === "table" ? (
-        <BooksTable books={books} />
-      ) : (
-        <BooksCard books={books} />
-      )}
     </div>
   );
 };
