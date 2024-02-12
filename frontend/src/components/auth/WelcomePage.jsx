54dbd2e5583cb2ced03d4229ef0f707d27864b06
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-12 20:19:12 +0800

    added bg design

--- frontend/src/components/auth/WelcomePage.jsx
+++ frontend/src/components/auth/WelcomePage.jsx
@@ -3,7 +3,7 @@ import img from "../img/logo.jpg";
 
 const WelcomePage = () => {
   return (
-    <div className="md:h-screen text-[#EFECEC] bg-[#39ad91] flex gap-4 justify-between md:justify-start items-center md:items-baseline md:flex-col w-screen md:w-[50%] px-4 py-2">
+    <div className="md:h-screen bg-my-background-image text-[#EFECEC] bg-[#39ad91] flex gap-4 justify-between md:justify-start items-center md:items-baseline md:flex-col w-screen md:w-[50%] px-4 py-2">
       <h2 className="text-1xl font-semibold">Ebook Haven</h2>
       <h1 className="text-2xl md:text-4xl mt-16 hidden md:block px-10 font-semibold">
         Discover a World of Stories at Your Fingertips
