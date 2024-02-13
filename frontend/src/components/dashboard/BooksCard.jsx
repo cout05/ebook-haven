72f5ffa2d3111df3826c5b5ead913f9e8c66985d
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-13 21:11:10 +0800

    adjusted

--- frontend/src/components/dashboard/BooksCard.jsx
+++ frontend/src/components/dashboard/BooksCard.jsx
@@ -2,7 +2,7 @@ import BookSingleCard from "./BookSingleCard";
 
 const BooksCard = ({ books }) => {
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
       {books.map((item) => (
         <BookSingleCard key={item._id} book={item} />
       ))}
