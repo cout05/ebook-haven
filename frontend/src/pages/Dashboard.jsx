7dcc0ab798cc76b22e4677bb4ea4683e22ffd6a4
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-13 21:14:15 +0800

    moved fetching books to upload component

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,47 +1,37 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Spinner from "../components/Spinner";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { MdOutlineAddBox } from "react-icons/md";
-import BooksCard from "../components/dashboard/BooksCard";
-import BooksTable from "../components/dashboard/BooksTable";
 import { IoChevronBack } from "react-icons/io5";
 import img from "../components/img/logo.jpg";
+import Uploaded from "../components/dashboard/Uploaded";
+import Library from "../components/dashboard/Library";
 
 const Dashboard = () => {
-  const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [showType, setShowType] = useState("table");
+  const [showType, setShowType] = useState(true);
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get("http://localhost:5555/books/")
-      .then((response) => {
-        setBooks(response.data.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setLoading(false);
-        console.log(error);
-      });
-  }, []);
   return (
-    <div className=" bg-my-background-image bg-gray-900 text-[#EFECEC] h-screen">
+    <div className="bg-my-background-image bg-gray-900 text-[#EFECEC]">
       <Link to="/" className="p-4">
         <IoChevronBack className="text-4xl cursor-pointer" />
       </Link>
-      <div className="flex gap-4">
-        <button className="text-2xl" onClick={() => setShowType("table")}>
+      <div className="flex gap-4 px-4 border-b-2 pb-2">
+        <button
+          onClick={() => setShowType(true)}
+          className={`${
+            showType ? "border-b-2 border-[#f04963]" : ""
+          } text-md font-semibold`}>
           Library
         </button>
-        <button className="text-2xl" onClick={() => setShowType("card")}>
+        <button
+          onClick={() => setShowType(false)}
+          className={`${
+            !showType ? "border-b-2 border-[#f04963]" : ""
+          } text-md font-semibold`}>
           Uploaded
         </button>
       </div>
 
-      <div className="flex justify-between gap-4">
-        <div className="p-10">
+      <div className="flex flex-col md:flex-row bg-my-background-image bg-[#39ad91] h-screen md:pt-8 px-4 md:justify-between md:gap-4">
+        <div className="w-full md:w-[30%] p-5 flex flex-col items-center">
           <div>
             <img
               className="w-[100px] h-[100px] rounded-full"
@@ -51,29 +41,7 @@ const Dashboard = () => {
           </div>
           <h1 className="text-2xl"> Welcome</h1>
         </div>
-        <div className="border w-full">
-          <div className="flex justify-between items-center px-4 border mb-8">
-            <h1 className="text-3xl">Uploaded</h1>
-            <div className="flex gap-4">
-              <button className="text-2xl" onClick={() => setShowType("table")}>
-                Table
-              </button>
-              <button className="text-2xl" onClick={() => setShowType("card")}>
-                Card
-              </button>
-            </div>
-            <Link to="/books/create">
-              <MdOutlineAddBox className="text-sky-800 text-4xl" />
-            </Link>
-          </div>
-          {loading ? (
-            <Spinner />
-          ) : showType === "table" ? (
-            <BooksTable books={books} />
-          ) : (
-            <BooksCard books={books} />
-          )}
-        </div>
+        {showType ? <Library /> : <Uploaded />}
       </div>
     </div>
   );
