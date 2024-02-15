d0e9bfa4a498e9e1ed7860cd8bfd4bcef6bbd4e8
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-15 21:55:39 +0800

    maps the latest uploaded book first

--- frontend/src/components/dashboard/BooksCard.jsx
+++ frontend/src/components/dashboard/BooksCard.jsx
@@ -2,8 +2,8 @@ import BookSingleCard from "./BookSingleCard";
 
 const BooksCard = ({ books }) => {
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
-      {books.map((item) => (
+    <div className="grid grid-cols-2 md:grid-cols-6 gap-2">
+      {books.reverse().map((item) => (
         <BookSingleCard key={item._id} book={item} />
       ))}
     </div>
