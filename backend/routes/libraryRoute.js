6d901d47c1e5c245be06d7950eb521ded54aa227
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-17 18:07:16 +0800

    imported bookSchema

--- backend/routes/libraryRoute.js
+++ backend/routes/libraryRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { Library } from "../models/libraryModels.js";
+import { Book } from "../models/bookModels.js";
 
 const router = express.Router();
 
@@ -31,8 +32,12 @@ router.post("/get", async (request, response) => {
         message: "Send all required fields: userId",
       });
     }
-    const saved = await Library.find({});
-    const books = saved.filter((e) => e.userId == request.body.userId);
+    const user = await Library.find({ userId: request.body.userId });
+    if (!user || user.length === 0) {
+      return response.status(404).send({ message: "User not found" });
+    }
+    const bId = user.map((e) => e.bookId); // Extracting book IDs
+    const books = await Book.find({ _id: { $in: bId } }); // Finding books by IDs
     return response.status(200).send({ books });
   } catch (error) {
     console.log(error.message);
