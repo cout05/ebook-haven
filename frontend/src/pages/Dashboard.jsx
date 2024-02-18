6c52ed27ad3c0603cc016b6e8987c967fe3eb0de
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-18 20:27:11 +0800

    updated the background color

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { IoChevronBack } from "react-icons/io5";
 import img from "../components/img/logo.jpg";
 import Uploaded from "../components/dashboard/Uploaded";
 import Library from "../components/dashboard/Library";
+import { AuthContext } from "../context/AuthContext";
 
 const Dashboard = () => {
+  const { userDetails } = useContext(AuthContext);
   const [showType, setShowType] = useState(true);
 
   return (
-    <div className="bg-my-background-image bg-gray-900 text-[#EFECEC]">
+    <div className="bg-my-background-image bg-[#39ad91]  text-[#EFECEC]">
       <Link to="/" className="p-4">
         <IoChevronBack className="text-4xl cursor-pointer" />
       </Link>
@@ -30,16 +32,25 @@ const Dashboard = () => {
         </button>
       </div>
 
-      <div className="flex flex-col md:flex-row bg-my-background-image bg-[#39ad91] h-screen md:pt-8 px-4 md:justify-between md:gap-4">
-        <div className="w-full md:w-[30%] p-5 flex flex-col items-center">
-          <div>
+      <div className="flex flex-col md:flex-row bg-my-background-image bg-gray-900 h-screen md:pt-8 px-4 md:justify-between md:gap-4">
+        <div className="w-full md:w-[30%] p-5 flex flex-row md:flex-col gap-4 md:gap-2">
+          <div className="">
             <img
-              className="w-[100px] h-[100px] rounded-full"
+              className="w-[100px] h-[100px]  md:min-w-[200px] md:min-h-[200px] bg-contain rounded-full"
               src={img}
               alt="logo"
             />
           </div>
-          <h1 className="text-2xl"> Welcome</h1>
+          <div className="flex flex-col gap-2 w-full flex-1">
+            <h1 className="text-2xl capitalize">
+              {userDetails.firstname} {userDetails.lastname}
+            </h1>
+            <p className="font-semibold">@{userDetails.username}</p>
+            <p>Bio</p>
+            <button className="px-2 py-1 rounded w-full bg-[#f04963]">
+              Edit Profile
+            </button>
+          </div>
         </div>
         {showType ? <Library /> : <Uploaded />}
       </div>
