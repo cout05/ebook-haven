5c1792351a67b28b06238bb523613454c6e8f349
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-19 21:22:26 +0800

    made the books responsive

--- frontend/src/components/home/Books.jsx
+++ frontend/src/components/home/Books.jsx
@@ -29,7 +29,7 @@ const Books = () => {
         {loading ? (
           <Spinner />
         ) : books.length > 0 ? (
-          <div className="grid grid-cols-4">
+          <div className="grid grid-cols-2 gap-4 md:gap-2 md:grid-cols-4 ">
             {books.map((book) => (
               <div key={book._id}>
                 <BookSingleCard book={book} />
