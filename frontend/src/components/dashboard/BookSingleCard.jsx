35ffade1ec69056cee566422e4420d3a74c27406
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-21 21:59:07 +0800

    removed unused imports, added ishome props

--- frontend/src/components/dashboard/BookSingleCard.jsx
+++ frontend/src/components/dashboard/BookSingleCard.jsx
@@ -1,14 +1,11 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { PiBookOpenTextLight } from "react-icons/pi";
-import { BiUserCircle, BiShow } from "react-icons/bi";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
-import { useState, useEffect } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import BookModal from "./BookModal";
 
-const BookSingleCard = ({ book }) => {
+const BookSingleCard = ({ book, isHome }) => {
   const [showModal, setShowModal] = useState(false);
   const [imageUrl, setImageUrl] = useState("");
 
@@ -28,17 +25,21 @@ const BookSingleCard = ({ book }) => {
 
   return (
     <div className="text-[#EFECEC] h-[200px] w-[150px] md:h-[300px] md:w-[200px] cursor-pointer rounded-lg  relative hover:shadow-2xl">
-      <div
-        onClick={() => setShowModal(!showModal)}
-        className="absolute z-10 right-1 top-3">
-        <BsThreeDotsVertical className="text-xl" />
-      </div>
+      {!isHome === true ? (
+        <div
+          onClick={() => setShowModal(!showModal)}
+          className="absolute z-10 right-1 top-3">
+          <BsThreeDotsVertical className="text-xl" />
+        </div>
+      ) : (
+        ""
+      )}
 
       <div
         className={`${
           showModal ? "flex" : "hidden"
         } absolute items-center justify-center gap-6  h-[200px] w-[150px] md:h-[300px] md:w-[200px] bg-opacity-50 bg-black transition-opacity duration-300`}>
-        <Link to={`/books/details/${book._id}`}>
+        <Link onClick={() => back("/")} to={`/books/details/${book._id}`}>
           <BsInfoCircle className="text-2xl text-[#39ad91]" />
         </Link>
         <Link to={`/books/edit/${book._id}`}>
