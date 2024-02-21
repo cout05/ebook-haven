7400c1926e32d6b6805671b7c53ec0b4095a3214
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-21 22:04:35 +0800

    fixed minor bugs

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -80,11 +80,11 @@ const CreateBooks = () => {
             />
           </div>
           <div className="flex flex-col gap-2">
-            <label class="block mb-2 text-sm font-medium " for="file_input">
+            <label className="block mb-2 text-sm font-medium ">
               Upload file
             </label>
             <input
-              class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+              className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
               id="file_input"
               type="file"
             />
