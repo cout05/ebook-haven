49f85b478a5767160aed0590080c417feaa87acd
Jerico <116884354+cout05@users.noreply.github.com>
2024-02-23 20:57:55 +0800

    fetch books details for carousel

--- frontend/src/components/home/CarouselComponent.jsx
+++ frontend/src/components/home/CarouselComponent.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import "tailwindcss/tailwind.css"; // Import Tailwind CSS
 import { Carousel } from "react-responsive-carousel"; // Import React Responsive Carousel CSS
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import React Responsive Carousel CSS
 
 const CarouselComponent = () => {
+  const [loading, setLoading] = useState(false);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5555/books/")
+      .then((response) => {
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="bg-gray-800  bg-my-background-image pt-[150px] pb-14">
       <div className="container mx-auto px-4">
         <div className="text-center">
           <h1 className="text-4xl font-bold text-white mb-4">
-            Discover Our Latest eBook Offers
+            Discover Our Latest Ebook Offers
           </h1>
           <p className="text-lg text-gray-300 mb-8">
             Explore our collection of eBooks with exclusive discounts!
@@ -24,36 +43,18 @@ const CarouselComponent = () => {
             showThumbs={false}
             showStatus={false}
             className="rounded-lg shadow-xl">
-            <div className="bg-white  rounded-lg p-8">
-              <h2 className="text-xl font-bold mb-2">eBook Title 1</h2>
-              <p className="text-gray-600 mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
-                tellus ut metus lobortis pulvinar.
-              </p>
-              <a href="#" className="text-blue-500 hover:text-blue-700">
-                Read More
-              </a>
-            </div>
-            <div className="bg-white rounded-lg p-8">
-              <h2 className="text-xl font-bold mb-2">eBook Title 2</h2>
-              <p className="text-gray-600 mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
-                tellus ut metus lobortis pulvinar.
-              </p>
-              <a href="#" className="text-blue-500 hover:text-blue-700">
-                Read More
-              </a>
-            </div>
-            <div className="bg-white rounded-lg p-8">
-              <h2 className="text-xl font-bold mb-2">eBook Title 3</h2>
-              <p className="text-gray-600 mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
-                tellus ut metus lobortis pulvinar.
-              </p>
-              <a href="#" className="text-blue-500 hover:text-blue-700">
-                Read More
-              </a>
-            </div>
+            {books.slice(0, 3).map((book, index) => (
+              <div key={index} className="bg-white rounded-lg p-8">
+                <h2 className="text-xl font-bold mb-2">{book.title}</h2>
+                <p className="text-gray-600 mb-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  et tellus ut metus lobortis pulvinar.
+                </p>
+                <Link className="text-blue-500 hover:text-blue-700">
+                  Read More
+                </Link>
+              </div>
+            ))}
           </Carousel>
         </div>
       </div>
